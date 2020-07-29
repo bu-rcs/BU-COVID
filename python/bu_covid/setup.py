329d921114e657df6424f6ef2d9f6e4615a853f5
Brian Gregor <bgregor@bu.edu>
2020-07-29 10:02:25 -0400

    setup.py is now configured and working. covasim can be downloaded from PyPI.

---
+++ python/bu_covid/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import os
+
+
+# This will install the bu_covid code along with covasim 1.4.7 
+# from PyPI and all necessary additional libraries.
+
+ 
+ 
+setup(name='bu_covid',
+      version='1.0',
+      description='BU code library for use with Covasim.',
+      author='BU',
+      author_email='bgregor@bu.edu',
+      url='https://github.com/bu-rcs/BU-COVID',
+      packages=['bu_covid', ],
+      package_dir={'bu_covid': 'src'},
+      install_requires=['covasim==1.4.7',],
+)
+
+ 
